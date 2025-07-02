064432460fcb9c671279bbdc50a6b3c433aca83c
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2025-07-02 14:47:11 +0700

    fixed#2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "App";
+import ReactDOM from "react-dom/client";
 
 // Soft UI Dashboard React Context Provider
 import { SoftUIControllerProvider } from "context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="he-thong-quan-ly-healthmate">
+
     <SoftUIControllerProvider>
       <App />
     </SoftUIControllerProvider>
-  </BrowserRouter>
+  
 );
